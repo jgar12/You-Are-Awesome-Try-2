89e46fdfe2a6fe111bee88d00d106296037880a6
Jordan Garcia <jordan.garcia@mcallenisd.net>
2022-01-18 08:14:16 -0600

    Completed 2.6

--- You Are Awesome! Try 2/ViewController.swift
+++ You Are Awesome! Try 2/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("☺️viewDidLoad has run!")
-        messageLabel.text="Fabulous, that's You!"
+
     }
 
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎The message button was pressed")
         messageLabel.text="You Are Awesome!"
+        imageView.image = UIImage(named: "image1")
     }
 }
 
